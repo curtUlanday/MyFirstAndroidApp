a141a2638b3b7070c48c8f057d38234f9e23d20f
Curt <ccaulanday@gmail.com>
2025-08-14 09:21:30 +0800

    Lifecyle Demo

--- app/src/main/java/com/example/myapp_nw2g/MainActivity.java
+++ app/src/main/java/com/example/myapp_nw2g/MainActivity.java
@@ -11,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,49 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Log.d("TEST", "onCreate called");
+        Log.d(TAG, "onCreate called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: Activity is now visible");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: Activity is now in the foreground and interactive");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: Activity is partially obscured, save temporary data");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: Activity is no longer visible");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: Activity is restarting after being stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: Activity is about to be destroyed");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("data", "This is some saved data");
+        Log.d(TAG, "onSaveInstanceState: Saved data before destruction");
     }
 }
